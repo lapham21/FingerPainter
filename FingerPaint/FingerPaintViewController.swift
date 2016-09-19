7afd9874eeaa3772c6a711265624f00ac6322a8e
lapham21 <nlapham21@yahoo.com>
2016-09-19 17:04:32 -0400

    wired all buttons and implemented the show or not show other buttons buttons functionality

--- FingerPaint/FingerPaintViewController.swift
+++ FingerPaint/FingerPaintViewController.swift
@@ -12,26 +12,75 @@ class FingerPaintViewController: UIViewController {
     
     // MARK: - Outlets, Variables and Models
     
+    var opacityButtonsShown = false
+    var paintBrushWidthButtonsShown = false
+    
+    @IBOutlet var opacitiyButtons: [UIButton]!
+    @IBOutlet var paintBrushWidthButtons: [UIButton]!
+
+    @IBAction func showOptions(_ sender: UIButton) {
+        
+        switch sender.tag {
+        case 0:
+            displayButtons(buttons: opacitiyButtons, withShouldShowBoolean: &opacityButtonsShown)
+()
+        case 1:
+            displayButtons(buttons: paintBrushWidthButtons, withShouldShowBoolean: &paintBrushWidthButtonsShown)
+        default: break
+        }
+        
+    }
+    
+    // TODO: - Change the opacity and width of drawline when buttons hit
+    @IBAction func opacityChangeButtons(_ sender: UIButton) {
+        switch sender.tag {
+        case 0: break
+        case 1: break
+        case 2: break
+        case 3: break
+        case 4: break
+        case 5: break
+        default: break
+        }
+    }
+    @IBAction func paintBrushWidthChangeButtons(_ sender: UIButton) {
+        switch sender.tag {
+        case 0: break
+        case 1: break
+        case 2: break
+        case 3: break
+        case 4: break
+        case 5: break
+        default: break
+        }
+    }
     
     
+    @IBAction func eraseButton(_ sender: UIButton) {
+        
+    }
     
-    // MARK: - Colors
     
-//    enum ColorPalette {
-//        case White =
-//        case DarkGreen = "51,58,24,255"
-//        case DarkGray = "64,48,56,255"
-//        case BrightWhite = "#ffffff"
-//        
-//        var color: UIColor {
-//            return RawValue.color
-//        }
-//    }
+    // MARK: - ViewController LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        displayButtons(buttons: opacitiyButtons, withShouldShowBoolean: &opacityButtonsShown)
+        displayButtons(buttons: paintBrushWidthButtons, withShouldShowBoolean: &paintBrushWidthButtonsShown)
+
+    }
+    
+    // MARK: - Utility Functions
+    
+    func displayButtons(buttons: [UIButton], withShouldShowBoolean shouldShow: inout Bool) {
+        for button in buttons {
+            button.isHidden = !shouldShow
+        }
+        shouldShow = !shouldShow
     }
 
+    
 }
 
