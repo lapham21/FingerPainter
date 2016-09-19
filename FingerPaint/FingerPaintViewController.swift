ad91eecad380e8b385ba963d6db3fcb4a84c14cc
lapham21 <nlapham21@yahoo.com>
2016-09-19 15:31:11 -0400

    first commit

---
+++ FingerPaint/FingerPaintViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  FingerPaint
+//
+//  Created by Nolan Lapham on 9/19/16.
+//  Copyright © 2016 Intrepid Pursuits. All rights reserved.
+//
+
+import UIKit
+
+class FingerPaintViewController: UIViewController {
+    
+    // MARK: - Colors
+    
+//    enum ColorPalette {
+//        case White =
+//        case DarkGreen = "51,58,24,255"
+//        case DarkGray = "64,48,56,255"
+//        case BrightWhite = "#ffffff"
+//        
+//        var color: UIColor {
+//            return RawValue.color
+//        }
+//    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
